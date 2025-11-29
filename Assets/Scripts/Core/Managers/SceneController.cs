477416c514a57d85951e956f1882b9281d81bd3e
hfuwcs <luuphuc588@gmail.com>
2025-11-29 16:51:22 +0700

    #

--- Assets/Scripts/Core/Managers/SceneController.cs
+++ Assets/Scripts/Core/Managers/SceneController.cs
@@ -5,6 +5,54 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private string mainMenuSceneName = "MainMenu";
 
+    private static string previousSceneName = "";
+    
+    public static string PreviousSceneName => previousSceneName;
+    
+    public static bool HasPreviousScene => !string.IsNullOrEmpty(previousSceneName);
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+    }
+
+
+    private static void SaveCurrentSceneAsPrevious()
+    {
+        previousSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    public void ReturnToPreviousScene()
+    {
+        if (HasPreviousScene)
+        {
+            Time.timeScale = 1f;
+            string sceneToLoad = previousSceneName;
+            previousSceneName = "";
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            ReturnToMainMenu();
+        }
+    }
+
+    public void LoadSceneWithHistory(string sceneName)
+    {
+        Time.timeScale = 1f;
+        SaveCurrentSceneAsPrevious();
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void ReturnToMainMenu()
     {
         Time.timeScale = 1f;
@@ -19,7 +67,13 @@ public class SceneController : MonoBehaviour
     public void PickCategory(string categoryName)
     {
         Time.timeScale = 1f;
+        SaveCurrentSceneAsPrevious();
         GameSettings.SelectedCategory = categoryName;
         SceneManager.LoadScene("QuizScene"); 
+    }
+
+    public static void ClearSceneHistory()
+    {
+        previousSceneName = "";
     }
 }
